5481ae8af896f4c9e12dc88fb202a1adccf5ba4a
LajasLeduop <53257572+LajasLeduop@users.noreply.github.com>
2023-09-08 11:48:19 +0545

    new changes rather than old ones

--- mockup_sunshine/mockup_sunshine/settings.py
+++ mockup_sunshine/mockup_sunshine/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p@1jkqqp-j-ox(f)+r#^b90td$+*&-2h2#pdl@*znjfk2w(0wt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
