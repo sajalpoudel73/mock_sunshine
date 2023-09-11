834714455116788677e4e2b7ff51123f7af75e3f
LajasLeduop <53257572+LajasLeduop@users.noreply.github.com>
2023-09-11 16:32:18 +0545

    changes made in database

--- mockup_sunshine/mockup_sunshine/settings.py
+++ mockup_sunshine/mockup_sunshine/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'mockup_sunshine.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mockup_sunshine',
+        'USER':'root',
+        'PASSWORD':'Sajal.2017',
+        'PORT':3306,
+        'HOST':'localhost'
     }
 }
 
