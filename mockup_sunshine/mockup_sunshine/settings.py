f72f9cbf9ddb0625354c88d25d59ca89159cac70
LajasLeduop <sajalpoudel73@gmail.com>
2023-12-02 17:37:37 +0545

    new commit from linux

--- mockup_sunshine/mockup_sunshine/settings.py
+++ mockup_sunshine/mockup_sunshine/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p@1jkqqp-j-ox(f)+r#^b90td$+*&-2h2#pdl@*znjfk2w(0wt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ '*' ]
 
